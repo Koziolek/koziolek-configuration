d04bc2708f4588a1f64db5f7429f94c20fa9f3db
Koziolek <bjkuczynski@gmail.com>
2025-04-17 14:45:38 +0200

    async_profiler sudo magic

--- bash/bash_functions.sh
+++ bash/bash_functions.sh
@@ -395,15 +395,15 @@ function resize_png() {
 
 function turn_async_profiler_on() {
   make_me_sudo
-  sh -c 'echo 1 > /proc/sys/kernel/perf_event_paranoid'
-  sh -c 'echo 0 > /proc/sys/kernel/kptr_restrict'
+  $SUDO sh -c 'echo 1 > /proc/sys/kernel/perf_event_paranoid'
+  $SUDO sh -c 'echo 0 > /proc/sys/kernel/kptr_restrict'
   unmake_me_sudo
 }
 
 function turn_async_profiler_off() {
   make_me_sudo
-  sh -c 'echo 4 > /proc/sys/kernel/perf_event_paranoid'
-  sh -c 'echo 1 > /proc/sys/kernel/kptr_restrict'
+  $SUDO sh -c 'echo 4 > /proc/sys/kernel/perf_event_paranoid'
+  $SUDO sh -c 'echo 1 > /proc/sys/kernel/kptr_restrict'
   unmake_me_sudo
 }
 
