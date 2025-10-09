fe056b151b271a8b954f8ea91e5409381c195929
Koziolek <bjkuczynski@gmail.com>
2025-10-09 15:02:34 +0200

    zmiany w ścieżkach

--- bash/bash_misc.sh
+++ bash/bash_misc.sh
@@ -13,4 +13,5 @@ shopt -s checkwinsize
 eval "$(thefuck --alias)"
 
 # jebana konfiguracja spotify 
-pactl set-default-sink alsa_output.usb-Razer_Razer_Kraken_Kitty_Edition_00000000-00.analog-stereo
+command -v pactl >/dev/null 2>&1 && pactl set-default-sink alsa_output.usb-Razer_Razer_Kraken_Kitty_Edition_00000000-00.analog-stereo
+
