a77b6eed91611c0664e6a7be2be6232fcc0c0420
Koziolek <bjkuczynski@gmail.com>
2025-10-09 16:19:44 +0200

    Dodany maven bash completion

--- bash/bash_customs.sh
+++ bash/bash_customs.sh
@@ -1,3 +1,8 @@
 install_lib -r "https://github.com/kward/shunit2.git" -t "shunit2" -e "shunit2.sh"
 install_lib -r "https://github.com/Koziolek/BashMan.git" -t "BashMan" -e "bashman.sh" -x
 install_lib -r "https://github.com/Koziolek/FossFLOW.git" -t "FossFLOW"
+install_lib -r "https://github.com/juven/maven-bash-completion.git" -t "maven-bash-completion"
+
+if [ ! -L ~/.maven-bash-completion ] && [ ! -d ~/.maven-bash-completion ]; then
+    ln -s $WORKSPACE_TOOLS/maven-bash-completion ~/.maven-bash-completion
+fi
